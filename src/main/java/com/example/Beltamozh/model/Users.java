63f4948c683860e127a6be715a1997461a9972d7
RemusLolol <artemsumskij89@gmail.com>
2023-12-14 23:56:55 +0300

    Данные из бд достаются, но не вставляются в html документ

--- src/main/java/com/example/Beltamozh/model/Users.java
+++ src/main/java/com/example/Beltamozh/model/Users.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "usersBD")
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/Beltamozh/repository/UsersRepository.java
+++ src/main/java/com/example/Beltamozh/repository/UsersRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    List<Users> findByUsername(String username);
 }
