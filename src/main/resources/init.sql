252f5942e72387939d5b5f3c0194edb2a04ae73e
RemusLolol <artemsumskij89@gmail.com>
2024-04-07 20:52:19 +0300

    Добавлены новые поля в таблицу

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -8,7 +8,9 @@ CREATE TABLE savesoperations(
     id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     typeTam VARCHAR(255) NOT NULL,
     tamposhl DECIMAL(5,2) NOT NULL,
-    transprash DECIMAL(5,2) NOT NULL,
+    ss DECIMAL(5,2) NOT NULL,
+    transprashdogra DECIMAL(5,2) NOT NULL,
+    transprashposlegra DECIMAL(5,2) NOT NULL,
     weightprod DECIMAL(5,2) NOT NULL,
     itogss DECIMAL(5,2) NOT NULL,
     itogssperweight DECIMAL(5,2) NOT NULl
