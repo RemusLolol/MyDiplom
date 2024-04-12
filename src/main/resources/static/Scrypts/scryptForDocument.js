c2d031e852501d3ea32faffd3e9a4fd15199fcb2
RemusLolol <artemsumskij89@gmail.com>
2024-04-12 12:54:25 +0300

    Изменена верстка сайта, добавлена функция расчета итогов по "Основным расчетам" при изменении полей ввода

--- src/main/resources/static/Scrypts/scryptForDocument.js
+++ src/main/resources/static/Scrypts/scryptForDocument.js
@@ -1,55 +1,71 @@
 let rateForSelectedCurrency;
 let date;
-(function getDataForApi() {
-    const selectElement = document.getElementById('currencySelector');
-    const ratesData = {};
+document.addEventListener('DOMContentLoaded', function () {
+    (function getDataForApi() {
+        const selectElement = document.getElementById('currencySelector');
+        const ratesData = {};
 
-    fetch('https://api.nbrb.by/exrates/rates?periodicity=0')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            data.forEach(currency => {
-                const optionElement = document.createElement('option');
-                optionElement.value = currency.Cur_Abbreviation;
-                optionElement.textContent = currency.Cur_Abbreviation;
-                selectElement.appendChild(optionElement);
+        fetch('https://api.nbrb.by/exrates/rates?periodicity=0')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                data.forEach(currency => {
+                    const optionElement = document.createElement('option');
+                    optionElement.value = currency.Cur_Abbreviation;
+                    optionElement.textContent = currency.Cur_Abbreviation;
+                    selectElement.appendChild(optionElement);
 
-                rateForSelectedCurrency = currency.Cur_OfficialRate;
-                date = currency.Date;
-                ratesData[currency.Cur_Abbreviation] = currency.Cur_OfficialRate;
-                console.log(currency.Cur_Abbreviation);
-                console.log(currency.Cur_OfficialRate);
-            });
-        })
-        .catch(error => console.error('Ошибка при получении данных:', error));
+                    rateForSelectedCurrency = currency.Cur_OfficialRate;
+                    date = currency.Date;
+                    ratesData[currency.Cur_Abbreviation] = currency.Cur_OfficialRate;
+                    console.log(currency.Cur_Abbreviation);
+                    console.log(currency.Cur_OfficialRate);
+                });
+            })
+            .catch(error => console.error('Ошибка при получении данных:', error));
 
-    selectElement.addEventListener('change', function () {
-        const selectedCurrency = this.value;
-        rateForSelectedCurrency = ratesData[selectedCurrency];
+        selectElement.addEventListener('change', function () {
+            const selectedCurrency = this.value;
+            rateForSelectedCurrency = ratesData[selectedCurrency];
 
-        var nac = rateForSelectedCurrency * parseFloat(document.getElementById("OsnForRasch11").value).toFixed(2);
-        document.getElementById('labelPeresch').innerText = "В НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: " + formattedDate(date) + "): " + nac.toFixed(2);
-        document.getElementById('labelPerechb').innerText = "(б) Косвенные платежи (условия или обязательства) в НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: "
-            + formattedDate(date) + ")";
-    });
-})();
+            var nac = rateForSelectedCurrency * parseFloat(document.getElementById("OsnForRasch11").value).toFixed(2);
+            if (isNaN(nac)) {
+                document.getElementById('labelPeresch').innerText = "В НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: " + formattedDate(date) + "): ";
+            }
+            else {
+                document.getElementById('labelPeresch').innerText = "В НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: " + formattedDate(date) + "): " +
+                    nac.toFixed(2);
+            }
+            document.getElementById('labelPerechb').innerText = "(б) Косвенные платежи (условия или обязательства) в НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: "
+                + formattedDate(date) + ")";
+        });
+    })();
+});
+
+function fillMainCalc(){
+    var nac = rateForSelectedCurrency * parseFloat(document.getElementById("OsnForRasch11").value).toFixed(2);
+    if (isNaN(nac)) {
+        document.getElementById('labelPeresch').innerText = "В НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: " + formattedDate(date) + "): ";
+    }
+    else {
+        document.getElementById('labelPeresch').innerText = "В НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: " + formattedDate(date) + "): " +
+            nac.toFixed(2);
+    }
+    document.getElementById('labelPerechb').innerText = "(б) Косвенные платежи (условия или обязательства) в НАЦИОНАЛЬНОЙ ВАЛЮТЕ (курс пересчета: "
+        + formattedDate(date) + ")";
+    const OsnForRasch11Value = parseFloat(document.getElementById("OsnForRasch11").value);
+    const OsnForRasch11bValue = parseFloat(document.getElementById("OsnForRasch11b").value);
+
+    let result;
+    if (isNaN(OsnForRasch11bValue)) {
+        result = ((OsnForRasch11Value * rateForSelectedCurrency)).toFixed(2);
+    } else {
+        result = (OsnForRasch11Value * rateForSelectedCurrency + parseFloat(OsnForRasch11bValue.toFixed(2))).toFixed(2);
+    }
+    document.getElementById("itogOsn").innerText = "12) Итого по разделам \"а\" и \"б\" графы 11 в национальной валюте: " + result;
+}
 
 function rashetVivod() {
-    //Расчет Основы для Расчета
-    const OsnForRasch11Value = parseFloat(document.getElementById("OsnForRasch11").value) || 0;
-    const OsnForRasch11bValue = parseFloat(document.getElementById("OsnForRasch11b").value) || 0;
-    // if(OsnForRasch11Value === 0 || OsnForRasch11bValue === 0){
-    //     validationMesDoc.innerText = "Пожалуйста, заполните все поля в пункте Основные расчеты";
-    //     validationMesDoc.style.backgroundColor = "#FF0000";
-    //     validationMesDoc.style.opacity = 1;
-    //     setTimeout(function () {
-    //         validationMesDoc.style.opacity = 0;
-    //     }, 2000);
-    //     return;
-    // }
-    const result = ((OsnForRasch11Value * rateForSelectedCurrency) + OsnForRasch11bValue).toFixed(2);
-    document.getElementById("itogOsn").innerText = "12) Итого по разделам \"а\" и \"б\" графы 11 в национальной валюте: " + result;
-
     //Расчет для Дополнительного начисления
     const values = [
         parseFloat(document.getElementById("Dop13").value) || 0,
@@ -73,15 +89,6 @@ function rashetVivod() {
             break;
         }
     }
-    // if (hasEmptyValue) {
-    //     validationMesDoc.innerText = "Пожалуйста, заполните все поля в пункте Дополнительные начисления";
-    //     validationMesDoc.style.backgroundColor = "#FF0000";
-    //     validationMesDoc.style.opacity = 1;
-    //     setTimeout(function () {
-    //         validationMesDoc.style.opacity = 0;
-    //     }, 2000);
-    //     return;
-    // }
     const totalDop = values.reduce((acc, currentValue) => acc + currentValue, 0);
     document.getElementById("itogoDop").innerText = "20) Итого по графам 13 - 19 в национальной валюте: " + totalDop.toFixed(2);
 
@@ -92,15 +99,6 @@ function rashetVivod() {
             break;
         }
     }
-    // if (hasEmptyValue) {
-    //     validationMesDoc.innerText = "Пожалуйста, заполните все поля в пункте Вычеты в национальной валюте";
-    //     validationMesDoc.style.backgroundColor = "#FF0000";
-    //     validationMesDoc.style.opacity = 1;
-    //     setTimeout(function () {
-    //         validationMesDoc.style.opacity = 0;
-    //     }, 2000);
-    //     return;
-    // }
 
     //Расчет Вычетов в национальной валюте
     const valuesVch = [
@@ -157,33 +155,10 @@ function download(blob, filename, mimetype) {
 
 async function fillDTSDocument() {
     try {
-        const osnForRasch11Value = parseFloat(document.getElementById("OsnForRasch11").value);
-        const osnForRasch11bValue = parseFloat(document.getElementById("OsnForRasch11b").value);
-        const osnForRasch12 = parseFloat(document.getElementById("itogOsn").value);
-
-        const dopForRasch13 = parseFloat(document.getElementById("Dop13b").value);
-        const dopForRasch14 = parseFloat(document.getElementById("Dop14").value);
-        const dopForRasch14a = parseFloat(document.getElementById("Dop14a").value);
-        const dopForRasch14b = parseFloat(document.getElementById("Dop14b").value);
-        const dopForRasch14v = parseFloat(document.getElementById("Dop14v").value);
-        const dopForRasch14g = parseFloat(document.getElementById("Dop14g").value);
-        const dopForRasch15 = parseFloat(document.getElementById("Dop15").value);
-        const dopForRasch16 = parseFloat(document.getElementById("Dop16").value);
-        const dopForRasch17 = parseFloat(document.getElementById("Dop17").value);
-        const dopForRasch18 = parseFloat(document.getElementById("Dop18").value);
-        const dopForRasch19 = parseFloat(document.getElementById("Dop19").value);
-        const itogDop = parseFloat(document.getElementById("itogoDop").value);
-
-        const vch21 = parseFloat(document.getElementById("Vch21").value);
-        const vch22 = parseFloat(document.getElementById("Vch22").value);
-        const vch23 = parseFloat(document.getElementById("Vch23").value);
-        const itogVch = parseFloat(document.getElementById("itogoVch").value);
-
-        const totalItog = parseFloat(document.getElementById(("itog25")).value);
-        const totalItogNac = parseFloat(document.getElementById(("itogNac")).value);
-
-        //validateInputValues(osnForRasch11Value, osnForRasch11bValue, osnForRasch12, dopForRasch13, dopForRasch14, dopForRasch14a, dopForRasch14b, dopForRasch14v, dopForRasch14g, dopForRasch15, dopForRasch16, dopForRasch17, dopForRasch18, dopForRasch19, itogDop, vch21, vch22, vch23, itogVch, totalItog, totalItogNac);
-
+        if (!checkAllAnswers()) {
+            console.error('Не все вопросы были отмечены.');
+            return;
+        }
         const pdfBytes = await fetch("/DTS-1.pdf").then(response => response.arrayBuffer());
         const pdfDoc = await PDFLib.PDFDocument.load(pdfBytes);
 
@@ -201,11 +176,25 @@ async function fillDTSDocument() {
     }
 }
 
-function validateInputValues(...values) {
-    const hasNullValues = values.some(value => value === null || isNaN(value));
-    if (hasNullValues) {
-        throw new Error('Пожалуйста, заполните все поля перед заполнением документа.');
+function checkAllAnswers() {
+    const questions = document.querySelectorAll('.question');
+    let answered = false;
+    for (let i = 0; i < questions.length; i++) {
+        const radios = questions[i].querySelectorAll('input[type="radio"]');
+        for (let j = 0; j < radios.length; j++) {
+            if (radios[j].checked === true) {
+                answered = true;
+                break;
+            }
+            else{
+                answered = false;
+            }
+        }
+        if (answered === false) {
+            return false;
+        }
     }
+    return true;
 }
 
 function modifyFirstPage(pdfDoc) {
@@ -269,19 +258,8 @@ function modifySecondPage(pdfDoc) {
     const secondPage = pdfDoc.getPages()[1];
     const fontSizeRasch = 10;
 
-    fillOsnRasch(secondPage, fontSizeRasch, formattedDate(date));
-
-    // secondPage.drawText(textRasch, {x:350, y:655, size:fontSizeRasch});//Сумма для 13 пункта
-    // secondPage.drawText(textRasch, {x:350, y:643, size:fontSizeRasch});//Сумма для 14 пункта
-    // secondPage.drawText(textRasch, {x:350, y:550, size:fontSizeRasch});//Сумма для 13 пункта
-    // secondPage.drawText(textRasch, {x:350, y:522, size:fontSizeRasch});//Сумма для 14 пункта
-    // secondPage.drawText(textRasch, {x:350, y:505, size:fontSizeRasch});//Сумма для 13 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 15 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 16 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 17 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 18 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 19 пункта
-    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Итог
+    //fillOsnRasch(secondPage, fontSizeRasch, formattedDate(date));
+    fillDopRasch(secondPage, fontSizeRasch);
 }
 
 function extractNumberFromString(str) {
@@ -309,6 +287,20 @@ function fillOsnRasch(secondPage, fontSizeRasch, formattedDate){
     secondPage.drawText(extractNumberFromString(document.getElementById("itogOsn").textContent), {x:350, y: 682, size:fontSizeRasch});
 }
 
+function fillDopRasch(secondPage, fontSizeRasch){
+    // secondPage.drawText(textRasch, {x:350, y:655, size:fontSizeRasch});//Сумма для 13 пункта
+    // secondPage.drawText(textRasch, {x:350, y:643, size:fontSizeRasch});//Сумма для 14 пункта
+    // secondPage.drawText(textRasch, {x:350, y:550, size:fontSizeRasch});//Сумма для 13 пункта
+    // secondPage.drawText(textRasch, {x:350, y:522, size:fontSizeRasch});//Сумма для 14 пункта
+    // secondPage.drawText(textRasch, {x:350, y:505, size:fontSizeRasch});//Сумма для 13 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 15 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 16 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 17 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 18 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Сумма для 19 пункта
+    // secondPage.drawText(textRasch, {x:350, y:455, size:fontSizeRasch});//Итог
+}
+
 function downloadPDF(pdfBytes) {
     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
     download(blob, 'dts-1(заполненный).pdf', 'application/pdf');
@@ -316,4 +308,82 @@ function downloadPDF(pdfBytes) {
 
 function openCalculator() {
     window.open('/calculator', '_blank');
+}
+
+
+function toggleSurvey() {
+    var surveyForm = document.getElementById("surveyContainer");
+    if (surveyForm.style.height === "0px") {
+        slideDown(surveyForm);
+    } else {
+        slideUp(surveyForm);
+    }
+}
+
+function toggleMainCalc(){
+    var surveyForm = document.getElementById("mainCalcContainer");
+    if (surveyForm.style.height === "0px") {
+        slideDown(surveyForm);
+    } else {
+        slideUp(surveyForm);
+    }
+}
+
+function toggleDopCalc(){
+    var surveyForm = document.getElementById("dobCalcContainer");
+    if (surveyForm.style.height === "0px") {
+        slideDown(surveyForm);
+    } else {
+        slideUp(surveyForm);
+    }
+}
+
+function toggleItog(){
+    var surveyForm = document.getElementById("itogContainer");
+    if (surveyForm.style.height === "0px") {
+        slideDown(surveyForm);
+    } else {
+        slideUp(surveyForm);
+    }
+}
+
+function slideDown(element) {
+    var height = 0;
+    var maxHeight = element.scrollHeight;
+    var duration = 500;
+    var interval = 10;
+
+    var timer = setInterval(function () {
+        height += (maxHeight / (duration / interval));
+        if (height >= maxHeight) {
+            clearInterval(timer);
+            element.style.height = "auto";
+        } else {
+            element.style.height = height + "px";
+        }
+    }, interval);
+}
+
+function slideUp(element) {
+    var height = element.scrollHeight;
+    var duration = 500;
+    var interval = 10;
+
+    var timer = setInterval(function () {
+        height -= (element.scrollHeight / (duration / interval));
+        if (height <= 0) {
+            clearInterval(timer);
+            element.style.height = "0px";
+            element.style.overflow = "hidden";
+        } else {
+            element.style.height = height + "px";
+        }
+    }, interval);
+}
+
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var burger = document.querySelector('.burger');
+    sidebar.classList.toggle('open');
+    burger.classList.toggle('open');
 }
